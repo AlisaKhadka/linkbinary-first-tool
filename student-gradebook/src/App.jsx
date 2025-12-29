98e568afe8f63560298ac7efb4dedeaa6478337d
AlisaKhadka <alisakhadka2@gmail.com>
2025-12-29 09:10:59 +0545

    Updated features

--- student-gradebook/src/App.jsx
+++ student-gradebook/src/App.jsx
@@ -1,18 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import StudentRow from "./components/StudentRow";
+
 const App = () => {
-  const [students, setStudents] = useState([]);
+  // Load students from localStorage
+  const [students, setStudents] = useState(() => {
+    const savedStudents = localStorage.getItem("students");
+    return savedStudents ? JSON.parse(savedStudents) : [];
+  });
+
   const [name, setName] = useState("");
   const [score, setScore] = useState("");
+  const [sortHighToLow, setSortHighToLow] = useState(true);
 
+  //  Save students to localstorage whenever students change
+  useEffect(() => {
+    localStorage.setItem("students", JSON.stringify(students));
+  }, [students]);
+
+  //  Add student
   const addStudent = () => {
+    if (!name || score === "") return alert("Enter name and score");
     setStudents([...students, { name: name, score: Number(score) }]);
     setName("");
     setScore("");
   };
 
+  // Update score
   const updateScore = (index) => {
     const newScore = prompt("Enter new score (0-100)");
+    if (newScore === null) return;
 
     setStudents(
       students.map((student, i) =>
@@ -20,6 +36,17 @@ const App = () => {
       )
     );
   };
+
+  // Sorting
+  const sortedStudents = [...students].sort((a, b) =>
+    sortHighToLow ? b.score - a.score : a.score - b.score
+  );
+
+  // Statistics
+  const total = students.length;
+  const passing = students.filter((s) => s.score >= 40).length;
+  const failing = total - passing;
+
   return (
     <div>
       <h3>Student List</h3>
@@ -44,10 +71,22 @@ const App = () => {
 
       <hr />
 
-      <StudentRow students={students} updateScore={updateScore} />
+      {/* Student List  */}
+      <StudentRow students={sortedStudents} updateScore={updateScore} />
+
+      {/* Sort Button */}
+      <div style={{ margin: "10px 0" }}>
+        <button onClick={() => setSortHighToLow(!sortHighToLow)}>
+          Sort: {sortHighToLow ? "High → Low" : "Low → High"}
+        </button>
+      </div>
+
+      {/* Statistics Bar */}
+      <div style={{ marginBottom: "10px" }}>
+        Total: {total} || Passing: {passing} || Failing: {failing}
+      </div>
     </div>
   );
 };
 
 export default App;
-
