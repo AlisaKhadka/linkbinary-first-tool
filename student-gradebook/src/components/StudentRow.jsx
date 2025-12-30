231f9db0a952bcdd29b5d5c1424e0eb7496f2d19
AlisaKhadka <alisakhadka2@gmail.com>
2025-12-30 17:22:12 +0545

    updated UI

--- student-gradebook/src/components/StudentRow.jsx
+++ student-gradebook/src/components/StudentRow.jsx
@@ -1,11 +1,19 @@
+import "../styles/StudentRow.css";
 const StudentRow = ({ students, updateScore }) => {
   return (
-    <div>
+    <div className="student-grid">
       {students.map((student, index) => (
-        <div key={index} style={{ marginBottom: "8px" }}>
-          {student.name} {student.score}
-          {student.score >= 40 ? " (Pass)" : " (Fail)"}
-          {student.score === 100 && " 🏆"}
+        <div key={index} className="student-card">
+          <div>
+            <strong>{student.name}</strong>
+          </div>
+          <div>
+            score: {student.score}
+            <span className={`status ${student.score >= 40 ? "pass" : "fail"}`}>
+              {student.score >= 40 ? "Pass" : "Fail"}
+            </span>
+            {student.score === 100 && " 🏆"}
+          </div>
           <button onClick={() => updateScore(index)}>Update Score</button>
         </div>
       ))}
