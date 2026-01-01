09fe20e578be5c41b124ece2a7a909866c3db7aa
AlisaKhadka <alisakhadka2@gmail.com>
2026-01-01 12:59:17 +0545

    added UI logic

--- api-dashboard/src/components/PhotoGallery.jsx
+++ api-dashboard/src/components/PhotoGallery.jsx
@@ -1,16 +1,18 @@
-import { useEffect, useState, } from "react";
-import "../styles/PhotoGallery.css"
+import { useEffect, useState } from "react";
+import PhotoCard from "./PhotoCard";
+import "../styles/PhotoGallery.css";
 
-   export default function PhotoGallery() {
-    const[photos, setPhotos] =useState([]);
-     const [loading, setLoading] = useState(true);
+export default function PhotoGallery() {
+  const [photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-useEffect(() => {
+  useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch('https://jsonplaceholder.typicode.com/photos');
+        const res = await fetch("https://jsonplaceholder.typicode.com/photos");
         const json = await res.json();
-        setPhotos(json.slice(0,50));
+          //  console.log("Fetched photos:", json.slice(0, 5));
+        setPhotos(json.slice(0, 50));
         setLoading(false);
       } catch (err) {
         console.error(err);
@@ -19,14 +21,28 @@ useEffect(() => {
 
     fetchData();
   }, []);
-return (
+  return (
     <div>
-<h1>Photo Gallery</h1>
-
+      <h1>Photo Gallery</h1>
       {loading ? (
-        <div className="skeleton-box">loading...</div>
+        < div className="grid">
+          {Array(50).fill(0).map((_, i) => (
+              <div key={i} className="skeleton-box"></div>
+            ))}
+        
+        </div>
       ) : (
-        <p>Total photos loaded: {photos.length}</p>
-      )}    </div>
+        <div className="grid">
+          {photos.map((photo) => (
+            <PhotoCard
+              key={photo.id} //key{i} is a skeleton ad this line is a real image
+               thumbnailUrl={photo.thumbnailUrl}
+              title={photo.title}
+              url={photo.url}
+              />
+          ))}
+        </div>
+      )}
+    </div>
   );
-   }
+}
